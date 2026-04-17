f872e95710c6ab9b5a46e608f4f34e68c4c3a0d9
linus-so <linsod070113@student.jenseneducation.se>
2026-04-17 11:28:25 +0200

    .öoilykevfwdc

--- scripts/header.js
+++ scripts/header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("header.html")
+  fetch("../docs/header.html")
     .then(res => res.text())
     .then(html => {
       document.getElementById("navbar").innerHTML = html;
